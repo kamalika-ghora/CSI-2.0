ea18338d81543159f4eecf8afc2f35ab2ea41753
raj nishad <rajnishad24082003@gmail.com>
2022-09-07 19:52:12 +0530

    first_commit

---
+++ by_raj/csi_working/script.js
@@ -0,0 +1,8 @@
+document.getElementById("toggle").addEventListener("click",()=>{
+  if(document.getElementById("text").style.display == "none")
+  {
+  document.getElementById("text").style.display = "flex";}
+  else{
+    document.getElementById("text").style.display = "none";
+  }
+})
