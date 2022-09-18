0516bd251bff263f59770d0aa40b3f70a2386869
Kamalika <59131714+kamalika-exe@users.noreply.github.com>
2022-09-18 16:26:33 +0530

    Update zephyrevents.js

--- zephyrevents.js
+++ zephyrevents.js
@@ -1,3 +1,8 @@
-document.querySelector(".button").addEventListener("click",()=>{
-
+document.getElementById("toggle").addEventListener("click",()=>{
+  if(document.getElementById("text").style.display == "none")
+  {
+  document.getElementById("text").style.display = "flex";}
+  else{
+    document.getElementById("text").style.display = "none";
+  }
 })
